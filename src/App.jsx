29cf5322308a79fa49946ac931d5ae590bce2d5a
Parbhat Sharma <prabhat29ps@gmail.com>
2024-10-26 11:10:04 +0530

    menu transition

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 
 function App() {
-
   return (
     <>
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,19 +15,31 @@ const Navbar = () => {
         <a href="/" aria-label="Homepage" className="font-oswald">
           <h2>P.S</h2>
         </a>
-        <div className="sm:flex hidden gap-5 font-poppins">
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#experience">Experience</a>
+        <div className="sm:flex sm:items-center hidden gap-5 font-poppins">
+          <div className="group">
+            <a href="#about">About</a>
+            <div className="h-[2px] bg-primaryText w-0 group-hover:w-full transition-all transform duration-300"></div>
+          </div>
+          <div className="group">
+            <a href="#projects">Projects</a>
+            <div className="h-[2px] bg-primaryText w-0 group-hover:w-full transition-all transform duration-300"></div>
+          </div>
+          <div className="group">
+            <a href="#experience">Experience</a>
+            <div className="h-[2px] bg-primaryText w-0 group-hover:w-full transition-all transform duration-300"></div>
+          </div>
         </div>
         <div className="relative sm:hidden flex">
           <button onClick={toggleMenuState}>
             {openMenu ? <IoClose aria-label="Close menu" size={22} /> : <AiOutlineMenu aria-label="Open menu" size={22} />}
           </button>
-          <div className={`${openMenu ? 'flex flex-col gap-2' : 'hidden'} rounded-md font-poppins absolute top-7 right-0 bg-white text-gray-800 p-3`}>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#experience">Experience</a>
+          <div className={`${openMenu ? 'max-h-40' : 'max-h-0'} flex flex-col gap-2 transition-all ease duration-300 rounded-md font-poppins absolute top-7 right-0 bg-white text-gray-800`}>
+            {openMenu && <>
+              <a className="pt-3 px-3" href="#about">About</a>
+              <a className="px-3" href="#projects">Projects</a>
+              <a className="pb-3 px-3" href="#experience">Experience</a>
+            </>
+            }
           </div>
         </div>
       </div>
